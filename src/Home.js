5888f1ac69c801586c1f890cd9838fce6d34970e
zhien95 <33336170+zhien92@users.noreply.github.com>
2020-12-11 15:48:49 +0800

    chatbot + homepage

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,28 @@
-
-function Home(){
-    return (
-      <>
-      <center><h1>Home Page</h1></center>
-        </>
-      )
-  }
-
-  export default Home;
+import React, { Component } from 'react';
+ 
+class Home extends Component {
+ render () {
+ return (
+ <div>
+ 
+ <center><h1>Home Page</h1></center>
+ 
+ <form>
+ <center>
+ <label>
+ <h2>Welcome to DBS e-Wallet </h2>
+ </label>
+ 
+ <label>
+ {/* OUR MISSION: Make Banking Joyful */}
+ </label>
+ <button type="About us">About Us</button>
+ <button type="FAQ">FAQ</button>
+ </center>
+ </form>
+ </div>
+ )
+ }
+}
+ 
+export default Home;
