d04379a22ffe98f71a64d65ebf10369a03dbb392
ismailishak <ismailishakmi@gmail.com>
2020-12-11 13:26:42 +0800

    added html ui for loginpage

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,24 @@
-import './index.css'
-
-export default function Login (props) {
-
-    return(        
-        <center>
-        <article>
-            <h1>Login</h1>
-        </article>
-        </center>
-    )   
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+class Login extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Login Page</h1>
+    
+                <form>
+                    <label>
+                        Username:
+                        <input name="username" type="text" placeholder="Username" required />
+                    </label>
+                    <label>
+                        Password:
+                        <input name="password" type="password" placeholder="Password" required />
+                    </label>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        )
+    }
 }
-
-
-
+export default withRouter(Login)
