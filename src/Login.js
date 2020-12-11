d2678021e4a4238ba54ae04d04f501a5c0b4f773
zhien95 <33336170+zhien92@users.noreply.github.com>
2020-12-11 13:13:02 +0800

    added respective files

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+export default function Login (props) {
+
+    return(        
+        <center>
+        <article>
+            <h1>Login</h1>
+        </article>
+        </center>
+    )   
+}
+
+
+
---
+++ src/Payment.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Payment extends Component {
+    
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Payment</h1>
+            </div>
+        )
+    }
+}
+
+export default Payment
