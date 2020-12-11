32ebc8db83451c80135ad9ecfa6bdb6d276ffc22
ismailishak <ismailishakmi@gmail.com>
2020-12-11 14:16:15 +0800

    new login

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './Login.css'
+
 class Login extends Component {
     render () {
         return (
-            <div className="login-card">
-                <h1>Login</h1>
-
+            <div className="login-card">                
                 <form>
                     <label>
                         Username:
+                        <br />
                         <input name="username" type="text" placeholder="Username" required />
                     </label>
+                    <br />
                     <label>
                         Password:
+                        <br />
                         <input name="password" type="password" placeholder="Password" required />
                     </label>
+                    <br />
                     <button type="submit">Login</button>
                 </form>
             </div>
