ee9341a4faf23f1c6a69937ccb079b4660a8ba8a
zhien95 <33336170+zhien92@users.noreply.github.com>
2020-12-11 16:19:02 +0800

    Merged changes by nathan and Beatrice

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,6 @@
-import React, { Component } from 'react';
- 
-class Home extends Component {
- render () {
- return (
- <div>
+function Home(){
+    return (
+<div>
  
  <center><h1>Home Page</h1></center>
  
@@ -21,8 +18,7 @@ class Home extends Component {
  </center>
  </form>
  </div>
- )
- }
-}
- 
-export default Home;
+      )
+  }
+
+  export default Home;
