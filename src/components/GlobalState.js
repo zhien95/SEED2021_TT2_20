053fdced095eb9e396d1b71a4c0a456cde29b1b3
zhien95 <33336170+zhien92@users.noreply.github.com>
2020-12-11 15:26:45 +0800

    nathan module

---
+++ src/components/GlobalState.js
@@ -0,0 +1,49 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+// Initial state
+const initialState = {
+  transactions: [],
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function resetTransaction() {
+    dispatch({
+      type: "RESET_TRANSACTION",
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+        resetTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+}; export default GlobalProvider
---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "./GlobalState";
+
+export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Total Send Amount</h4>
+        <p className="money minus">{income}</p>
+      </div>
+    </div>
+  );
+};
+
+
+export default IncomeExpenses;
